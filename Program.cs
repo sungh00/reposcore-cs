964fdbff8104bf3832e4551b41ef4d29902d8ef4
Ki Yung Ahn <kyagrd@gmail.com>
2025-04-16 19:15:50 +0000

    Got rid of too much indentation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿using Cocona;
 
-CoconaApp.Run(([Argument] string[] repository, bool verbose) =>
-        {
-            Console.WriteLine($"Repository: {String.Join("\n ", repository)}");
-            if (verbose)
-            {
-                Console.WriteLine("Verbose mode is enabled.");
-            }
-        });
+CoconaApp.Run(([Argument] string[] repository, bool verbose) => {
+    Console.WriteLine($"Repository: {String.Join("\n ", repository)}");
+    if (verbose) {
+        Console.WriteLine("Verbose mode is enabled.");
+    }
+});
