c075c6c233e56cfe3f1368cd3288cf6264c262d5
AnhyunJun1741 <elo8721@naver.com>
2025-04-23 16:33:14 +0000

    GitHub API URL 기능 추가

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using Cocona;
-// using System;
-//using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
 
-CoconaApp.Run((
+CoconaApp.Run(async(
     [Argument] string[] repos,
     [Option('v', Description = "자세한 로그 출력을 활성화합니다.")] bool verbose
 ) =>
@@ -20,6 +23,35 @@ CoconaApp.Run((
         Environment.Exit(1);  // 오류 발생 시 exit code 1로 종료
         return;
     }
+
+    string owner = repos[0];
+    string repo = repos[1];
+
+    string Url = "https://api.github.com/repos";
+    string apiUrl = $"{Url}/{owner}/{repo}";
+
+
+    try
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("CoconaApp", "1.0"));
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+            string result = await response.Content.ReadAsStringAsync();
+
+            //var jsonElement = JsonDocument.Parse(result).RootElement;
+            //var prettyJson = JsonSerializer.Serialize(jsonElement, new JsonSerializerOptions { WriteIndented = true });
+
+            Console.WriteLine($"[INFO]API 응답 코드: {response.StatusCode}");
+            //Console.WriteLine($"[INFO]API 응답 내용:\n{prettyJson}");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"❗ 오류 발생: {e.Message}");
+        Environment.Exit(1);
+    }
+
 
     try
     {
