8cd9e67fc409c73ee52835f2dd439a39123e666e
Ki Yung Ahn <kyagrd@gmail.com>
2025-04-23 19:45:01 +0900

    변수 이름 간소화

--- Program.cs
+++ Program.cs
@@ -3,18 +3,18 @@
 //using System.Threading.Tasks;
 
 CoconaApp.Run((
-    [Argument] string[] repository,
+    [Argument] string[] repos,
     [Option('v', Description = "자세한 로그 출력을 활성화합니다.")] bool verbose
 ) =>
 {
-    Console.WriteLine($"Repository: {String.Join("\n ", repository)}");
+    Console.WriteLine($"Repository: {String.Join("\n ", repos)}");
 
     if (verbose)
     {
         Console.WriteLine("Verbose mode is enabled.");
     }
 
-    if (repository.Length != 2)
+    if (repos.Length != 2)
     {
         Console.WriteLine("❗ repository 인자는 'owner repo' 순서로 2개가 필요합니다.");
         Environment.Exit(1);  // 오류 발생 시 exit code 1로 종료
@@ -24,7 +24,7 @@ CoconaApp.Run((
     try
     {
         // var analyzer = new GitHubAnalyzer();
-        // analyzer.Analyze(repository[0], repository[1]);
+        // analyzer.Analyze(repos[0], repos[1]);
     }
     catch (Exception ex)
     {
