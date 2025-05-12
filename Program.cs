bd4f325e93923dcc6c424cf58285f711e0cd497c
박주은 <parkjueuny@gmail.com>
2025-05-12 13:14:54 +0900

    빈파일 생성2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ using Octokit;
 CoconaApp.Run((
     [Argument] string[] repos,
     [Option('v', Description = "자세한 로그 출력을 활성화합니다.")] bool verbose,
-    [Option("o|output", Description = "출력 디렉토리 경로를 지정합니다.")] string? output,
-    [Option("f|format", Description = "출력 형식을 지정합니다. (예: json,csv)")] string? format,
+    [Option('o', Description = "출력 디렉토리 경로를 지정합니다.")] string? output,
+    [Option('f', Description = "출력 형식을 지정합니다. (예: json,csv)")] string? format,
     [Option('t', Description = "GitHub Personal Access Token 입력")] string? token
 ) =>
 {
