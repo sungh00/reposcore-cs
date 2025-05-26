777843055c0566d265bfe4b23ab9bcbefa452f79
Ki Yung Ahn <kyagrd@gmail.com>
2025-05-26 15:46:10 +0900

    DummyData 파일 이름 위치 수정

--- Reposcore/CommonData.cs
+++ Reposcore/CommonData.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 
 // 깃헙에서 우리가 필요한 정보를 가져와서 담아놓는 레코드 (각 내역별 활동 횟수)
-public record UserActivity( // 나중에 적당한 다른 곳으로 옮겨야 함
+public record UserActivity(
     int PR_fb,
     int PR_doc,
     int PR_typo,
@@ -9,9 +9,19 @@ public record UserActivity( // 나중에 적당한 다른 곳으로 옮겨야 �
     int IS_doc
 );
 
+// UserActivity를 분석해서 사용자별 점수를 계산하는 레코드
+public record UserScore(
+    // int ????, // 점수의 이름은 나중에 정하기,
+    // int ????, // 점수의 이름은 나중에 정하기,
+    // int ????, // 점수의 이름은 나중에 정하기,
+    // int ????, // 점수의 이름은 나중에 정하기,
+    // int ????, // 점수의 이름은 나중에 정하기,
+);
+
 // 1번 단계를 책임지는 Repscore/RepoDataCollector.cs의 클래스의 객채 하나가
 // 모아오는 데이타가 바로 repo1Activities 같은 것이다.
-public static class DummyData {
+public static class DummyData
+{
     public static Dictionary<string, UserActivity> repo1Activities = new() {
         { "user00", new UserActivity(1, 0, 0, 0, 0) },
         { "user01", new UserActivity(0, 1, 0, 0, 0) },
@@ -36,3 +46,36 @@ public static class DummyData {
         { "user12", new UserActivity(16, 3, 12, 8, 14) },
     };
 }
+
+/*
+1단계 RepoDataCollector
+
+하나의 저장소를 담당하는 객체 하나마다
+Dictionary<String, UserActivity> 값 1개씩 만들어냄
+          사용자이름  활동건수
+
+Dictionary<String, UserActivity> 이거가 이름이 기니까
+이거를 줄인 이름을 만드는 것도???
+          
+2단계 ***Analyzer
+
+이것도 꼭 여러 개를 분석한다는 개념이 아니라
+그냥 하나의 정보를 분석한다고 치고
+Dictionary<String, UserActivity> 이거 한 개 넘겨받아서 분석하고
+여러 번 각 저장소마다 분석하고
+
+파이썬이나 JS쪽처럼 대략 하면 될 듯
+ 
+어떤 데이터 구조를 생성해야 하냐? 대략 이름이 UserScore 같은
+점수 구성요소들을 정라하는 레코드/구조체 타입을 정의해야 함 
+
+여기 단계에서 하는 일을 데이터 구조 중심으로 정리하자면
+사용자별 UserActivity를 보고 사용자별 UserScore를 만들어냄
+
+그러니까 ***Analyzer객체가 만들어내는 데이터는 대략
+Dictionary<String, UserScore> 가 된다는 말
+
+ 
+3단계 ....
+....
+*/
